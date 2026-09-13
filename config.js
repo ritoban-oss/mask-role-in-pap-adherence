54a8298c7be72629df2475edb3ea328bfc5fefaa
ritoban-oss <ritoban@nutgraf.agency>
2026-09-13 07:22:07 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Paste your deployed Apps Script /exec URL below, then push this file to your repo.
 // See README.md for the deployment steps.
-const SCRIPT_URL = "PASTE_YOUR_APPS_SCRIPT_URL_HERE";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbz4xv7et7fL1DcWkqncGVR8UdunOGhjaLGUq-uglIGSEwZvnMaymHFZBv7r_ElIsUZn/exec";
